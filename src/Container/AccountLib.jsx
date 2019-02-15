2d2d1b3f07f71e89dd4131d17dfe5e69e894d5a8
ymeliberty <yeimi.mejias@eliberty.fr>
2019-02-15 10:43:21 +0100

    dev en cours

--- src/Container/AccountLib.jsx
+++ src/Container/AccountLib.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import {PropTypes} from "prop-types";
 import ContactInformation from '../Components/ContactInformation/ContactInformation';
 import FormAccountEdit from '../Components/FormAccountEdit/FormAccountEdit';
+import AccountForm from "../../../../../identification/src/Components/AccountForm/AccountForm";
 
 const AccountLib = props => (
   <div>
     <ContactInformation
       data={props.data}
     />
-    <Collapse isOpened={this.props.render.get('openCreationAccount', false)}>
-      <FormAccountEdit
-        data={props.data}
-        onClickEditAccount={props.onClickEditAccount}
-        setAddress={this.props.setAddress}
-        setFirstname={this.props.setFirstname}
-        setLastname={this.props.setLastname}
-        setMobile={this.props.setMobile}
-        setOptIn={this.props.setOptIn}
-        setPhone={this.props.setPhone}
-        setEmail={this.props.setEmail}
-        setBirthdate={this.props.setBirthdate}
-        setPassword={this.props.setPassword}
-      />
-    </Collapse>
+
+    {/*<Collapse isOpened={this.props.render.get('openCreationAccount', false)}>*/}
+    <FormAccountEdit
+      data={props.data}
+      // onClickEditAccount={props.onClickEditAccount}
+      // setAddress={this.props.setAddress}
+      // setFirstname={this.props.setFirstname}
+      // setLastname={this.props.setLastname}
+      // setMobile={this.props.setMobile}
+      // setOptIn={this.props.setOptIn}
+      // setPhone={this.props.setPhone}
+      // setEmail={this.props.setEmail}
+      // setBirthdate={this.props.setBirthdate}
+      // setPassword={this.props.setPassword}
+    />
+    {/*</Collapse>*/}
   </div>
 );
 
